2fd6cf6bafe4c6ffb27c7af24a00c37a3669adae
tenspot <mervyn.posnett@gmail.com>
2018-07-03 11:02:54 +0100

    latest update

--- javascript/this.js
+++ javascript/this.js
@@ -5,21 +5,31 @@
 // this keyword - refers to an object that is set at the creation of a new 
 // execution context (function invocation)
 
-// Example
+// Example of 'this' keyword
 const person = {
     name: 'John',
     greet: function() { console.log( 'Hello, ' + this.name) },
 }
 
 // prints 'John'
-person.greet
+person.greet()
 
-// prints 
+// prints 'undefined'
 const greet = person.greet
+greet()
 
 const friend = {
     name: 'David',
 }
-
+friend.greet = person.greet
 // prints 'David'
 friend.greet()
+
+/* To set 'this' can use bind(), call() or apply()
+so can write:
+    const greet = person.greet.bind( {name: 'text to print'} )
+    person.greet.call( {name: 'text to print'} )
+    person.greet.apply( {name: 'text to print'} )
+
+    Note also the '=>' notation 
+*/
